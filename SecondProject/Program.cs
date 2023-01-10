9a600ddb672b36d5f07d08d6aacaf3956df209e1
Dhananjay <dmmohokar94@gmail.com>
2023-01-10 16:42:48 +0530

    This is Basic .NET Framework written with C#

---
+++ SecondProject/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SecondProject
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("welcome");
+            Console.WriteLine("to");
+            Console.WriteLine("my project");
+            Console.ReadKey();
+
+            Console.Clear();
+            Console.WriteLine("Thank you so much");
+         Console.ReadKey();
+        }
+    }
+}
